51a82cfa947e2f4ce33093d6f20c929cd02dd9f0
Lammert Westerhoff <westerhoff@gmail.com>
2023-06-13 11:20:52 -0600

    Update environment variables

--- EvervaultIOSApp/EvervaultIOSApp/EvervaultIOSAppApp.swift
+++ EvervaultIOSApp/EvervaultIOSApp/EvervaultIOSAppApp.swift
@@ -13,8 +13,8 @@ import EvervaultCore
 struct EvervaultIOSAppApp: App {
 
     init() {
-        let teamId: String = nil ?? ProcessInfo.processInfo.environment["VITE_EV_TEAM_UUID"]! // replace nil with teamId or specify teamId as environment variable
-        let appId: String = nil ?? ProcessInfo.processInfo.environment["VITE_EV_APP_UUID"]! // replace nil with appId or specify teamId as environment variable
+        let teamId: String = nil ?? ProcessInfo.processInfo.environment["EV_TEAM_UUID"]! // replace nil with teamId or specify teamId as environment variable
+        let appId: String = nil ?? ProcessInfo.processInfo.environment["EV_APP_UUID"]! // replace nil with appId or specify teamId as environment variable
         Evervault.shared.configure(teamId: teamId, appId: appId)
     }
 
