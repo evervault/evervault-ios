2fa161e15232f1e7c36111494ea8693e8e0c2878
Lammert Westerhoff <westerhoff@gmail.com>
2023-06-13 09:59:27 -0600

    Update mock configuration

--- gen-mocks.sh
+++ gen-mocks.sh
@@ -3,6 +3,6 @@ set -eu
 cd "$(dirname "$0")"
 swift package describe --type json > project.json
 .build/checkouts/mockingbird/mockingbird generate --project project.json \
-  --output-dir Tests/EvervaultTests/MockingbirdMocks \
-  --testbundle EvervaultTests \
-  --targets Evervault
+  --output-dir Tests/EvervaultCoreTests/MockingbirdMocks \
+  --testbundle EvervaultCoreTests \
+  --targets EvervaultCore
