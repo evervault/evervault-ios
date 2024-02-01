b2e5d2c9a6586bc83c0c1b29ab165cf0e99a36b9
Eoin Boylan <jb.eoin@gmail.com>
2024-02-01 16:02:42 +0000

    bugfix: nsnumber are not encrypted

--- Sources/EvervaultCore/core/dataHandlers/NumberHandler.swift
+++ Sources/EvervaultCore/core/dataHandlers/NumberHandler.swift
@@ -5,7 +5,22 @@ internal struct NumberHandler: DataHandler {
     let encryptionService: EncryptionService
 
     func canEncrypt(data: Any) -> Bool {
-        data is any Numeric
+        return data is Int ||
+               data is Int8 ||
+               data is Int16 ||
+               data is Int32 ||
+               data is Int64 ||
+               data is UInt ||
+               data is UInt8 ||
+               data is UInt16 ||
+               data is UInt32 ||
+               data is UInt64 ||
+               data is Float ||
+               data is Double ||
+               data is CGFloat ||
+               data is Decimal ||
+               data is NSNumber &&
+               !(data is Bool)
     }
 
     func encrypt(data: Any, context: DataHandlerContext) throws -> Any {
