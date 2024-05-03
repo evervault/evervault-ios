e5d139d03c413037f69f4628efca58ff6f677419
Eoin Boylan <jb.eoin@gmail.com>
2024-05-03 16:05:38 +0100

    chore: changeset

--- EvervaultIOSApp/EvervaultIOSApp/Views/CreditCardCustomView.swift
+++ EvervaultIOSApp/EvervaultIOSApp/Views/CreditCardCustomView.swift
@@ -1,62 +1,55 @@
-//
-//  SwiftUIView.swift
-//  
-//
-//  Created by Eoin Boylan on 03/05/2024.
-//
-
 import SwiftUI
 import EvervaultInputs
 
 struct CreditCardCustomView: View {
     @State private var cardData = PaymentCardData()
-    
+
     private var enabledFields = EnabledFields(isCardNumberEnabled: true, isExpiryEnabled: false, isCVCEnabled: false)
-    
+
     var body: some View {
         Form {
             Section("Payment card input") {
                 PaymentCardInput(cardData: $cardData, fields: enabledFields)
             }
-            
+
             Section("Payment card data") {
                 VStack(alignment: .leading, spacing: 4) {
                     Text("Card number:")
                         .bold()
                     Text(cardData.card.number)
                 }
-                
+
                 VStack(alignment: .leading, spacing: 4) {
                     Text("Bin:")
                         .bold()
                     Text(cardData.card.bin)
                 }
-                
+
                 VStack(alignment: .leading, spacing: 4) {
                     Text("Last 4:")
                         .bold()
                     Text(cardData.card.lastFour)
                 }
-                
-                
+
+
                 VStack(alignment: .leading, spacing: 4) {
                     Text("Card type:")
                         .bold()
                     Text(cardData.card.type?.brand ?? "")
                 }
-                
+
                 VStack(alignment: .leading, spacing: 4) {
                     Text("Is valid:")
                         .bold()
                     Text(cardData.isValid ? "Yes" : "No")
                 }
-                
+
                 VStack(alignment: .leading, spacing: 4) {
                     Text("Is potentially valid:")
                         .bold()
                     Text(cardData.isPotentiallyValid ? "Yes" : "No")
                 }
-                
+
                 if let error = cardData.error {
                     VStack(alignment: .leading, spacing: 4) {
                         Text("Error message:")
--- Sources/EvervaultInputs/Model/EnabledFields.swift
+++ Sources/EvervaultInputs/Model/EnabledFields.swift
@@ -1,10 +1,3 @@
-//
-//  File.swift
-//  
-//
-//  Created by Eoin Boylan on 03/05/2024.
-//
-
 import Foundation
 
 /// Configuration for validating payment card data fields.
@@ -12,7 +5,7 @@ public struct EnabledFields: Equatable {
     var isCardNumberEnabled: Bool = true
     var isExpiryEnabled: Bool = true
     var isCVCEnabled: Bool = true
-    
+
     public init(isCardNumberEnabled: Bool = true, isExpiryEnabled: Bool = true, isCVCEnabled: Bool = true) {
         self.isCardNumberEnabled = isCardNumberEnabled
         self.isExpiryEnabled = isExpiryEnabled
